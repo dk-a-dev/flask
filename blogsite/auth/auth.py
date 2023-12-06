b1faed8dbbcb2641984b66dfa8272cb88767f4f7
Dev Keshwani <dev.keshwani345@gmail.com>
2023-12-06 12:17:50 +0530

    feat: check for user login enabled

--- blogsite/auth/auth.py
+++ blogsite/auth/auth.py
@@ -15,16 +15,18 @@ from blogsite.database.db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
+
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('user_id')
+    user_id = session.get("user_id")
 
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
+
 
 @bp.route("/register", methods=("GET", "POST"))
 def register():
@@ -68,19 +70,30 @@ def login():
         user = db.execute("SELECT *FROM user WHERE email=?", (email,)).fetchone()
 
         if user is None:
-            error='No user with this email'
-        elif not check_password_hash(user['password'],password):
-            error= f'Incorrect password for {user["email"]}'        
+            error = "No user with this email"
+        elif not check_password_hash(user["password"], password):
+            error = f'Incorrect password for {user["email"]}'
 
         if error is None:
             session.clear()
-            session['user_id']=user['id']
-            return redirect(url_for('index'))
-        
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
         flash(error)
-    return render_template('auth/login.html')
-        
-@bp.route('logout')
+    return render_template("auth/login.html")
+
+
+@bp.route("logout")
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        return view(**kwargs)
+
+    return wrapped_view
