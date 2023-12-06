9642567b3b2627df1ec2cc032874f684b3e135c6
Dev Keshwani <dev.keshwani345@gmail.com>
2023-12-06 11:27:15 +0530

    refactor: auto formatted all the files

--- blogsite/auth/auth.py
+++ blogsite/auth/auth.py
@@ -20,29 +20,29 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 def register():
     if request.method == "POST":
         username = request.form["username"]
-        password = request.form['password']
-        email=request.form['email']
-        db=get_db()
-        error=None
+        password = request.form["password"]
+        email = request.form["email"]
+        db = get_db()
+        error = None
 
         if not username:
-            error='Username is required'
+            error = "Username is required"
         elif not password:
-            error='Password is required'
+            error = "Password is required"
         elif not email:
-            error='Email is required'
-        
+            error = "Email is required"
+
         if error is None:
             try:
                 db.execute(
                     "INSERT INTO user(email,username,password) VALUES (?,?,?)",
-                    (email,username,generate_password_hash(password)),
+                    (email, username, generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
-                error=f"User:{username} is already registered."
+                error = f"User:{username} is already registered."
             else:
                 return redirect(url_for("auth.login"))
-        
+
         flash(error)
-    return render_template('auth/register.html')
+    return render_template("auth/register.html")
